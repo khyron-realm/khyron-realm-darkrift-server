67d04cf5b2a3488b91397b9d3e8a15b45a30a9c0
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-10-18 10:17:17 +0300

    Updated unlimited dll with working tasks

---
+++ Update.ps1
@@ -0,0 +1,2 @@
+#!/bin/bash
+yes | cp /Users/alex_grigoras/Documents/GitHub/Unlimited-NetworkingServer-MiningGame/Unlimited-NetworkingServer-MiningGame/bin/Debug/Unlimited_NetworkingServer_MiningGame.dll /Users/alex_grigoras/Documents/GitHub/Unlimited-DarkriftServer-MiningGame/Plugins/Unlimited_NetworkingServer_MiningGame.dll
---
+++ UpdateRun.ps1
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yes | cp /Users/alex_grigoras/Documents/GitHub/Unlimited-NetworkingServer-MiningGame/Unlimited-NetworkingServer-MiningGame/bin/Debug/Unlimited_NetworkingServer_MiningGame.dll /Users/alex_grigoras/Documents/GitHub/Unlimited-DarkriftServer-MiningGame/Plugins/Unlimited_NetworkingServer_MiningGame.dll
+
+dotnet Lib/DarkRift.Server.Console.dll
